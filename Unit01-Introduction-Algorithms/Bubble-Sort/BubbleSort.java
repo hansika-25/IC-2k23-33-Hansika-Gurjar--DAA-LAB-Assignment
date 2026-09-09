e79b10691846ac8afc34166792f5e772905dc088
hansika-25 <hansikagurjar5@gmail.com>
2026-09-09 23:43:32 +0530

    Implement Bubble Sort

---
+++ Unit01-Introduction-Algorithms/Bubble-Sort/BubbleSort.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    // Method to perform Bubble Sort
+    public static void bubbleSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // If no swapping occurred, array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    // Method to print the array
+    public static void printArray(int[] arr) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input size
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input elements
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        // Apply Bubble Sort
+        bubbleSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+
+        sc.close();
+    }
+}
